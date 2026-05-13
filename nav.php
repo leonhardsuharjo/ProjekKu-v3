8c3c714a1a99b306da65c8d208e7f6526b27be76
Leonhard Satria Suharjo <leonhard.suharjo@gmail.com>
2026-05-13 18:51:04 +0200

    u

---
+++ nav.php
@@ -0,0 +1,11 @@
+<nav>
+    <a class="nav-brand" href="index.php">Project Manager</a>
+    <a href="index.php">Home</a>
+    <a href="customers.php">Customers</a>
+    <a href="suppliers.php">Suppliers</a>
+    <a href="products.php">Products</a>
+    <a href="materials.php">Materials</a>
+    <a href="jobroles.php">Job Roles</a>
+    <a href="projects.php">Projects</a>
+    <a href="insight.php">Insight</a>
+</nav>
